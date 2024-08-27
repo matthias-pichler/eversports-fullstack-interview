d2dbb0c28cb7c46f3de97549c15f0d266c980bd6
Matthias Pichler <matthias@pichler.co.com>
2024-08-27 17:57:36 +0200

    feat: add ValidationError

--- src/error-handler.middleware.ts
+++ src/error-handler.middleware.ts
@@ -1,8 +1,17 @@
 import type { ErrorRequestHandler } from "express";
+import { ValidationError } from "./modern/errors";
+import { StatusCodes } from "http-status-codes";
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
 	console.error(err.stack);
-	res.status(500).json({
+
+	if (err instanceof ValidationError) {
+		return res.status(StatusCodes.BAD_REQUEST).json({
+			message: err.message,
+		});
+	}
+
+	res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
 		error: "Internal Server Error",
 		message: err.message,
 	});
