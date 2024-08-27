3a0268f15c17cdf37ca7d07f661496b289f35b97
Matthias Pichler <matthias@pichler.co.com>
2024-08-27 16:02:57 +0200

    feat: add list memberships usecase

---
+++ src/modern/domain/memberships/usecases/list-memberships.ts
@@ -0,0 +1,14 @@
+import type { IMembership } from "../entities/membership";
+import type { IMembershipRepository } from "../repositories/membership-repository";
+
+export class ListMemberships {
+  private readonly repository: IMembershipRepository;
+
+  public constructor(repository: IMembershipRepository) {
+    this.repository = repository;
+  }
+
+  public execute = async (): Promise<IMembership[]> => {
+    return this.repository.listMemberships();
+  };
+}
