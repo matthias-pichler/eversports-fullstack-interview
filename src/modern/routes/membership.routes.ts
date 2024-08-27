c68de1e5b90a31ddcc331be0dabf6220d0f79a53
Matthias Pichler <matthias@pichler.co.com>
2024-08-27 16:03:19 +0200

    feat: implement list memberships route

--- src/modern/routes/membership.routes.ts
+++ src/modern/routes/membership.routes.ts
@@ -1,11 +1,30 @@
 import express, { type Request, type Response } from "express";
 import membershipPeriods from "../../data/membership-periods.json";
 import memberships from "../../data/memberships.json";
+import type { IMembershipRepository } from "../domain/memberships/repositories/membership-repository";
+import { JsonMembershipRepository } from "../domain/memberships/repositories/json-membership-repository";
+import { ListMemberships } from "../domain/memberships/usecases/list-memberships";
+import { StatusCodes } from "http-status-codes";
 
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-	throw new Error("not implemented");
+const membershipRepository: IMembershipRepository =
+	new JsonMembershipRepository({
+		memberships: memberships,
+		membershipPeriods: membershipPeriods,
+	});
+
+router.get("/", async (req: Request, res: Response) => {
+	const useCase = new ListMemberships(membershipRepository);
+
+	const memberships = await useCase.execute();
+
+	const result = memberships.map(({ periods, ...membership }) => ({
+		membership,
+		periods,
+	}));
+
+	res.status(StatusCodes.OK).json(result);
 });
 
 router.post("/", (req: Request, res: Response) => {
